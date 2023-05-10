a985a2d66a75b39e40bdd556ce7e7de60109174a
jmilljr24 <jmilljr24@gmail.com>
2023-05-10 11:16:30 -0400

    Changed @head/@tail to use node instead of value
    previous main file renamed to old.rb temporarily

--- old.rb
+++ old.rb
@@ -15,16 +15,27 @@ class LinkedList
     @tail = nil
   end
 
+  def new_node(value, next_node)
+    @node = Node.new(value, next_node)
+  end
+
   def prepend_data(value)
-    new_node = Node.new(value)
     if @head.nil?
-      @head = new_node
-      @tail = new_node
+      new_node(value, nil)
+      @head = value
+      @tail = value
     else
-      new_node.next_node = @head.value
-      @tail = @head
-      @head = new_node
+      new_node(value, head)
+      @head = value
     end
+  end
+
+  def append_data(value)
+    p @tail.next
+    # @tail.next = value
+    new_node(value, nil)
+    @tail = value
+    #   self.head = new_node(value, head)
   end
 end
 
@@ -33,5 +44,9 @@ one = LinkedList.new
 one.prepend_data('justin')
 one.prepend_data('tamara')
 one.prepend_data('arlo')
+one.prepend_data('lou')
+one.append_data('bob')
 
+number_nodes = ObjectSpace.each_object(Node).count
+puts number_nodes
 ObjectSpace.each_object(Node) { |i| p i }
