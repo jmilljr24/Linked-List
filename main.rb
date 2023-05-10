e1eadb47f9f7bce7729d9495c67acaf5ddd96e31
jmilljr24 <jmilljr24@gmail.com>
2023-05-10 11:32:36 -0400

    Add append method to LinkedList

--- main.rb
+++ main.rb
@@ -22,16 +22,28 @@ class LinkedList
       @tail = new_node
     else
       new_node.next_node = @head.value
-      @tail = @head
       @head = new_node
+    end
+  end
+
+  def append_data(value)
+    new_node = Node.new(value)
+    if @head.nil?
+      @head = new_node
+      @tail = new_node
+    else
+      @tail.next_node = new_node.value
+      @tail = new_node
     end
   end
 end
 
 one = LinkedList.new
 
-one.prepend_data('justin')
-one.prepend_data('tamara')
-one.prepend_data('arlo')
+one.prepend_data(3)
+one.prepend_data(4)
+one.prepend_data(5)
+one.append_data(2)
+one.append_data(1)
 
 ObjectSpace.each_object(Node) { |i| p i }
